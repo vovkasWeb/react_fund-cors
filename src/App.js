cf08ae059c530a8c7d43e5400d51564da2404c1c
vovkaWeb <vova77557@gmail.com>
2022-12-02 13:57:04 +0200

    react01

---
+++ src/App.js
@@ -0,0 +1,24 @@
+/** @format */
+
+import React, { useState } from "react";
+
+function App() {
+ const  [likes,setLikes] = useState(5);
+
+function increment(){
+setLikes(likes+1);
+} 
+function decrement(){
+	setLikes(likes-1);
+}
+
+  return (
+    <div className="App">
+      <h1>{likes}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
