dbc43f23eddcb84dc730b5596547d958e2246666
vovkaWeb <vova77557@gmail.com>
2022-12-02 16:44:13 +0200

    new update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+/** @format */
+
 import React, { useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import "./styles/App.css";
 
 function App() {
- const [value, setValue] = useState("Текст В Инпуте");
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaSript", body: "Desription" },
+    { id: 2, title: "Java", body: "D887" },
+    { id: 3, title: "Java", body: "D887" },
+  ]);
 
+  const [posts2, setPosts2] = useState([
+	{ id: 1, title: "JavaSript2", body: "Desription" },
+	{ id: 2, title: "Java2", body: "D887" },
+	{ id: 3, title: "Java2", body: "D887" },
+ ]);
 
   return (
     <div className="App">
-   <ClassCounter/>
+      {/* <PostItem post={{ id: 1, title: "JavaSript", body: "Desription" }} />
+		<PostItem post={{ id: 2, title: "Java", body: "D887" }} /> */}
+      <PostList posts={posts} title="Cписок постов 1"/>
+		<PostList posts={posts2} title="Cписок постов 2"/>
     </div>
   );
 }
